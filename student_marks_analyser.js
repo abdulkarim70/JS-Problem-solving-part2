09bd56b1d1a53ad0290626359bb250fdda4fb53b
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-02-05 15:02:19 +0600

    student marks completed

--- student_marks_analyser.js
+++ student_marks_analyser.js
@@ -1,12 +1,29 @@
 function analyseMarks(marksObj){
      let total=0
-        for(let key in marksObj){   
-              let value=marksObj[key]    
-                 total=total+value
-            }   
+let highestMarks= -Infinity;
+let lowestMarks= Infinity;
+let highestSubject=null;
+let lowestSubjest=null;
+
+        for(let subject in marksObj){   
+              let mark=marksObj[subject]    
+                 total=total+mark
+                 if(mark>highestMarks){
+                  highestMarks=mark;
+                  highestSubject=subject;
+                 }
+                 if(mark<lowestMarks){
+                  lowestMarks=mark;
+                  lowestSubjest=subject;
+                 }
+            } 
+            console.log('highest Marks',':',highestMarks)  
+            console.log('lowest Marks', ':',lowestMarks)  
+            console.log('highest subject', highestSubject)
+            console.log('lowest marks', lowestSubjest)
             let average=total/Object.keys(marksObj).length;
             console.log(average)
-            return{total, average}
+            return{total, average, highestSubject, lowestSubjest}
 }
 
 let output=analyseMarks({math:78,english:65,physics:88,bangla:55});
